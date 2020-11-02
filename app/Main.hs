bc810fafc1d0e05fb1917456b14df0345d61c4a7
Will Heron <willheron99@gmail.com>
2020-11-02 14:28:32 +0000

    Implemented parsing of leaderboard data

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,6 @@ main :: IO ()
 main = do
     let url = "https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5"
     json <- makeAPIRequest url
-    print json
+    case (parseLeaderboard json) of 
+        Left err -> print err
+        Right leaderboard -> print.head $ (entries leaderboard)
--- src/Parse.hs
+++ src/Parse.hs
@@ -1,5 +1,44 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Parse
-    ( parse,
+    ( parseLeaderboard,
+      Leaderboard(entries)
     ) where
 
-parse = putStrLn "TODO"
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as L8
+import GHC.Generics
+
+-- Create record types for leaderboard:
+-- Overall leaderboard type
+data Leaderboard = Leaderboard {
+    tier :: String,
+    leagueId :: String,
+    queue :: String,
+    name :: String,
+    entries :: [LeagueEntry]
+} deriving (Show, Generic)
+
+instance FromJSON Leaderboard
+instance ToJSON Leaderboard
+
+-- Individual player entries on this leaderboard
+data LeagueEntry = LeagueEntry {
+    summonerId :: String,
+    summonerName :: String,
+    leaguePoints :: Int,
+    rank :: String,
+    wins :: Int,
+    losses :: Int,
+    veteran :: Bool,
+    inactive :: Bool,
+    freshBlood :: Bool,
+    hotStreak :: Bool
+} deriving (Show, Generic)
+
+instance FromJSON LeagueEntry
+instance ToJSON LeagueEntry
+
+-- Create parser for handling leaderboard JSON data.
+parseLeaderboard :: L8.ByteString -> Either String Leaderboard
+parseLeaderboard json = eitherDecode json :: Either String Leaderboard
