504a455bf012d8e4cc4cdc3ad611429b1d9bd009
Will Heron <willheron99@gmail.com>
2020-11-21 13:03:31 +0000

    Created function to request an individual matches data

--- src/HTTPS.hs
+++ src/HTTPS.hs
@@ -11,7 +11,8 @@ module HTTPS
     (
       makeAPIRequest,
       requestSummonerByName,
-      requestMatchList
+      requestMatchList,
+      requestMatchData
     ) where
 
 -- Import needed packages
@@ -63,5 +64,20 @@ requestSummonerByName name = do
 requestMatchList :: String -> IO L8.ByteString
 requestMatchList accountId = do
   req <- parseRequest $ "https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/" ++ accountId ++ "?endIndex=10&api_key=" ++ api_key
+  res <- httpLBS req
+  return $ getResponseBody res
+
+
+{- |
+  'requestMatchData' takes a match id, and attempts to query the match endpoint for detailed match
+  information. 
+
+  This function takes an 'Int' parameter - which is the match ID to request.
+
+  This function returns an 'IO' 'L8.ByteString', containing all of the match details.
+-}
+requestMatchData :: Int -> IO L8.ByteString
+requestMatchData game_id = do
+  req <- parseRequest $ "https://euw1.api.riotgames.com/lol/match/v4/matches/" ++ show(game_id) ++ "?api_key=" ++ api_key
   res <- httpLBS req
   return $ getResponseBody res
